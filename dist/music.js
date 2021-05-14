9c47dcdd8f0685a830a5a759a1d61320ea4e9002
Kiddy-Sky <2502053485@qq.com>
2021-05-15 00:45:51 +0800

    Site updated: 2021-05-15 00:45:20

--- dist/music.js
+++ dist/music.js
@@ -9,37 +9,37 @@ const ap = new APlayer({
       {
         name: "Yesterday Once More",
         artist: 'Carpenters',
-        url: 'https://kiddy-Sky.github.io/dist/songs/Yesterday Once More -- Carpenters .mp3', 
+        url: '/dist/songs/Yesterday Once More -- Carpenters .mp3', 
       },
       {
         name: "斯德哥尔摩情人",
         artist: '陈奕迅',
-        url: 'https://kiddy-Sky.github.io/dist/songs/斯德哥尔摩情人 -- 陈奕迅 .mp3', 
+        url: '/dist/songs/斯德哥尔摩情人 -- 陈奕迅 .mp3', 
       },
       {
         name: '少女的祈祷',
         artist: '杨千嬅',
-        url: 'https://Kiddy-Sky.github.io/dist/songs/少女的祈祷 -- 杨千嬅 .mp3',
+        url: '/dist/songs/少女的祈祷 -- 杨千嬅 .mp3',
       },
       {
         name: '钟无艳',
         artist: '谢安琪',
-        url: 'https://Kiddy-Sky.github.io/dist/songs/钟无艳 -- 谢安琪 .mp3',
+        url: '/dist/songs/钟无艳 -- 谢安琪 .mp3',
       },
       {
         name: '倒刺',
         artist: '薛凯琪',
-        url: 'https://Kiddy-Sky.github.io/dist/songs/倒刺 -- 薛凯琪 .mp3',
+        url: '/dist/songs/倒刺 -- 薛凯琪 .mp3',
       },
       {
         name: '厌弃',
         artist: '许廷铿',
-        url: 'https://Kiddy-Sky.github.io/dist/songs/厌弃 -- 许廷铿 .mp3',
+        url: '/dist/songs/厌弃 -- 许廷铿 .mp3',
       },
       {
         name: '阁楼',
         artist: '陈慧琳',
-        url: 'https://Kiddy-Sky.github.io/dist/songs/阁楼 -- 陈慧琳 .mp3',
+        url: '/dist/songs/阁楼 -- 陈慧琳 .mp3',
       //url: 'http://music.163.com/song/media/outer/url?id=213791.mp3',
       }
     ]
